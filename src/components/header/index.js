1c32952c6684a7b6fa42c97924a3fd4791b5226f
Keivon <keivonproctor@hotmail.com>
2021-06-24 01:16:49 +0100

    first layout

---
+++ src/components/header/index.js
@@ -0,0 +1,13 @@
+
+import './header.css';
+
+
+export default function Header() {
+    return(
+      <div>
+      <ul className='navbar'>
+        <li><span>Crypto Track</span></li>
+      </ul>
+    </div>
+    )
+  }
