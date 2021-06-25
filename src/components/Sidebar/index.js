bf55fdde34aa769d671bdf443c48d107c9534e7f
Keivon <keivonproctor@hotmail.com>
2021-06-25 23:36:23 +0100

    added search in the side menu

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,14 +4,42 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
 
+function myFunction() {
+  // Declare variables
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("mySearch");
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myMenu");
+  li = ul.getElementsByTagName("li");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].getElementsByTagName("i")[0];
+    if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
+
 function Sidebar() {
   return (
     <div id="mySidenav" className="sidenav">
   <i className="closebtn" onClick={() => {closeNav()}}>&times;</i>
-  <i>About</i>
-  <i>Services</i>
-  <i >About</i>
-  <i >Services</i>
+  <input type="text" id="mySearch" onKeyUp={() => {myFunction()}} 
+  placeholder="Search.." title="Type in a cion"></input>
+ <div id="myMenu">
+  <li><i>HTML</i></li>
+  <li><i >CSS</i></li>
+  <li><i >JavaScript</i></li>
+  <li><i >PHP</i></li>
+  <li><i >Python</i></li>
+  <li><i >jQuery</i></li>
+  <li><i >SQL</i></li>
+  <li><i >Bootstrap</i></li>
+  <li><i >Node.js</i></li>
+</div>
 </div>
   );
 }
